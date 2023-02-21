ab6a374b4f9ef035937acb4dda89e9f90e4adbe3
Pamela Namala <namala.pamela@gmail.com>
2023-02-21 14:38:00 +0300

    Replaced add function with multiply

--- play.py
+++ play.py
@@ -1,6 +1,11 @@
-def add(a, b):
-    return a + b
+# def add(a, b):
+    # return a + b
+    # 
+    # print(add(4, 5))
+    #print(add(11, 22))
+def multiply(a, b):
+    return a * b
+
+print(multiply(11, 22))
 
 
-print(add(4, 5))
-print(add(11, 22))
