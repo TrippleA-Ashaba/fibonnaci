2a41ddda7db629f9f773587b93b0ae50171c6511
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-21 14:52:59 +0300

    TDD1 failing

--- play.py
+++ play.py
@@ -1,11 +1,2 @@
-# def add(a, b):
-    # return a + b
-    # 
-    # print(add(4, 5))
-    #print(add(11, 22))
-def multiply(a, b):
-    return a * b
-
-print(multiply(11, 22))
-
-
+def fibonnaci(n):
+    
--- test/test.js
+++ test/test.js
@@ -1,1 +1,7 @@
 import { assert } from 'chai';
+
+describe('Testing the Fibonnaci Function', () => {
+  it('Failing test of fibonnaci', () => {
+    assert.equal(fibonnaci(4), 6);
+  });
+});
