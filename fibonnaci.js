281eb2e4101ca79c00ad5eae8ff33366af380ce1
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-21 16:14:24 +0300

    Added comment of authors

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,10 +1,11 @@
-export default function fibonacci(n) {
-if (n < 1){
-  return('n cannot be 0 or less than 0')
-} else if (n === 1 || n === 2) {
-  return 1;
-} else {
+// Albert Ashaba Aheebwa - Namala Pamela
 
-  return fibonacci(n - 1) + fibonacci(n - 2)
-}
+export default function fibonacci(n) {
+  if (n < 1) {
+    return 'n cannot be 0 or less than 0';
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 }
