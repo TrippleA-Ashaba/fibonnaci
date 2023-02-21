88a380d09a663fc4b3f33175e359147944d103b0
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-21 15:23:54 +0300

    fibo with python

--- play.py
+++ play.py
@@ -1,2 +1,15 @@
-def fibonnaci(n):
-    
+def fibonacci(n):
+    a = 1
+    b = 1
+    c = 0
+    # print(a)
+    # print(b)
+    for i in range(2, n):
+        c = a + b
+        # print(c)
+        a = b
+        b = c
+    return c
+
+
+print(fibonacci(10))
