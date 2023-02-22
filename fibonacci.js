c9348721b06f09de592c51710bf649eed3d3b2d1
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-22 12:07:16 +0300

    final commit

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,8 @@
 // Albert Ashaba Aheebwa - Namala Pamela
 
 export default function fibonacci(n) {
-  if (n < 1) {
-    return 'n cannot be 0 or less than 0';
+  if (n < 1 || n % 1 !== 0) {
+    return 'n must be a positive integer';
   } else if (n === 1 || n === 2) {
     return 1;
   } else {
--- test/test.js
+++ test/test.js
@@ -13,31 +13,31 @@ describe.skip('Testing the fibonacci Function', () => {
   });
 });
 
-describe('Testing the first fibonacci Function', () => {
+describe('Testing for the 1st fibonacci', () => {
   it('Returning 1 as the first fibonacci', () => {
     assert.equal(fibonacci(1), 1);
   });
 });
 
-describe('Testing the second fibonacci Function', () => {
+describe('Testing for the 2nd fibonacci', () => {
   it('Returning 1 as the second fibonacci', () => {
     assert.equal(fibonacci(2), 1);
   });
 });
 
-describe('Testing the third fibonacci Function', () => {
+describe('Testing for the 3rd fibonacci', () => {
   it('Returning 2 as the third fibonacci', () => {
     assert.equal(fibonacci(3), 2);
   });
 });
 
-describe('Testing the fifth fibonacci Function', () => {
+describe('Testing for the 5th fibonacci', () => {
   it('Returning 5 as the fifth fibonacci', () => {
     assert.equal(fibonacci(5), 5);
   });
 });
 
-describe('Testing the tenth fibonacci Function', () => {
+describe('Testing for the 10th fibonacci', () => {
   it('Returning 55 as the tenth fibonacci', () => {
     assert.equal(fibonacci(10), 55);
   });
@@ -45,6 +45,12 @@ describe('Testing the tenth fibonacci Function', () => {
 
 describe('Testing for handling negative values', () => {
   it('Returning error message for negative numbers', () => {
-    assert.equal(fibonacci(-3), 'n cannot be 0 or less than 0');
+    assert.equal(fibonacci(-3), 'n must be a positive integer');
+  });
+});
+
+describe('Testing for handling fractional values', () => {
+  it('Returning error message for negative numbers', () => {
+    assert.equal(fibonacci(7.4), 'n must be a positive integer');
   });
 });
